de93ad78fe30c204c061b6d451fcbf0710630ded
Fernando Cocco <fernandoagustincocco@gmail.com>
2022-04-01 20:00:23 -0300

    Initial commit

---
+++ src/main/java/com/facrod/prodemundial/ProdeMundialApplication.java
@@ -0,0 +1,13 @@
+package com.facrod.prodemundial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdeMundialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProdeMundialApplication.class, args);
+    }
+
+}
